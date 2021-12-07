012b66e10e4effe6e809a6ad5a8d16bc91e35487
mehekthaker <mehekthaker@gmail.com>
2021-12-07 16:40:20 -0600

    Added backend service call and embedded grafana

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
+import { ApiService } from './api.service';
 import { Component } from '@angular/core';
 import { formatDate } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { WeatherParams } from './weather-params';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +14,17 @@ export class AppComponent {
   title = 'weather-proj';
   today = new Date();
   todayStr = '';
-  constructor(){
+  jsonData : WeatherParams = {'temperature': '', 'brightness': '', 'pressure': '', 'humidity': ''};
+
+
+  constructor(private apiService: ApiService){
     this.todayStr = formatDate(this.today, 'EEEE, MMMM d, y', 'en-US', '+0530');
+  }
+
+  getResponse(): void{
+    this.apiService.getResponsePayload().subscribe( res => {
+      console.log(res);
+      this.jsonData = res;
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
+import { ApiService } from './api.service';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { TrendsComponent } from './trends/trends.component';
 import { AppRoutingModule } from './app-routing.module';
 
 const appRoutes: Routes = [
-  { path: 'weather-trends', component: TrendsComponent },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' }
+  { path: 'weather-trends', component: TrendsComponent }
 ];
 
 @NgModule({
@@ -23,9 +26,11 @@ const appRoutes: Routes = [
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     ),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
