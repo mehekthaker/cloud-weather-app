fcd895a6563c520f0825013e0b8c1167ee0ec6fa
mehekthaker <mehekthaker@gmail.com>
2021-12-09 17:48:54 -0600

    Modified routing between pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { ApiService } from './api.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { formatDate } from '@angular/common';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { WeatherParams } from './weather-params';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +11,28 @@ import { WeatherParams } from './weather-params';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'weather-proj';
   today = new Date();
   todayStr = '';
-  jsonData : WeatherParams = {'temperature': '', 'brightness': '', 'pressure': '', 'humidity': ''};
 
+  jsonData : WeatherParams = {'temperature': '0.0', 'brightness': '0.0', 'pressure': '0.0', 'humidity': '0.0'}
+  //setWeatherData = {'temperature': '19.12', 'brightness': '0.04', 'pressure': '20.88', 'humidity': '84.1'}
 
   constructor(private apiService: ApiService){
-    this.todayStr = formatDate(this.today, 'EEEE, MMMM d, y', 'en-US', '+0530');
+    this.todayStr = formatDate(this.today, 'EEEE, MMMM d, y', 'en-US', '-0600');
   }
 
-  getResponse(): void{
+  ngOnInit(): void{
     this.apiService.getResponsePayload().subscribe( res => {
       console.log(res);
       this.jsonData = res;
     });
   }
+
+  // setWeatherParams(): void{
+  //   const setJsonData = {temperature: this.setWeatherData.temperature, brightness: this.setWeatherData.brightness, pressure: this.setWeatherData.pressure, humidity: this.setWeatherData.humidity};
+  //   //const data = JSON.stringify(setJsonData);
+  //   this.apiService.postResponse(setJsonData);
+  // }
 }
